68a67b8339781ffa6440fc826d84a9da743dbb56
Aung Kyaw Htet <akhtet@yahoo.com>
2016-04-09 00:10:56 +1000

    Display user messages, wait for 1 min before prompting to shutdown

--- aidr-single/start_ec2_instance.py
+++ aidr-single/start_ec2_instance.py
@@ -1,6 +1,12 @@
 
+import sys, time
 import boto.ec2
+
+DRY_RUN=False
+
 conn = boto.ec2.connect_to_region("ap-southeast-2")
+
+print "Starting new ec2 instance."
 
 res = conn.run_instances(
 	#Ubuntu Server 14.04 LTS (PV),EBS General Purpose (SSD) Volume Type
@@ -8,7 +14,19 @@ res = conn.run_instances(
 
         key_name='bcloud-key-1',
 	
-	#$0.08 per Hour
         instance_type='t2.medium',
 
-        security_groups=['bcloud-sg-1'])
+        security_groups=['bcloud-sg-1'],
+
+	dry_run=DRY_RUN)
+
+for s in range(0, 60):
+	print "\rWaiting for", 60-s, "seconds",
+	sys.stdout.flush()
+	time.sleep(1)
+
+for i in  res.instances:
+	print "\ninstance-id:", i.id
+
+raw_input("Press ENTER to terminate instance when ready.")
+res.stop_all(dry_run=DRY_RUN)
